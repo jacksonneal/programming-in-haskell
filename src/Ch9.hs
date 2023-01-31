2f6182c3af8ddcd9158b19d37032880487575eca
jacksonneal <jacksontneal@gmail.com>
2023-01-31 09:26:52 -0500

    Ch9 brute force

--- src/Ch9.hs
+++ src/Ch9.hs
@@ -58,3 +58,26 @@ choices = concatMap perms . subs
 solution :: Expr -> [Int] -> Int -> Bool
 solution e ns n =
   elem (values e) (choices ns) && eval e == [n]
+
+split :: [a] -> [([a], [a])]
+split [] = []
+split [_] = []
+split (x : xs) = ([x], xs) : [(x : ls, rs) | (ls, rs) <- split xs]
+
+exprs :: [Int] -> [Expr]
+exprs [] = []
+exprs [n] = [Val n]
+exprs ns = [e | (ls, rs) <- split ns, l <- exprs ls, r <- exprs rs, e <- combine l r]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+ops :: [Op]
+ops = [Add, Sub, Mul, Div]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n =
+  [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+main :: IO ()
+main = print (solutions [1, 3, 7, 10, 25, 50] 765)
