aa7fbf2bdc97e1e65e0dca9021916bf128a45b0b
jacksonneal <jacksontneal@gmail.com>
2023-02-17 00:02:48 -0500

    life

--- src/Ch10.hs
+++ src/Ch10.hs
@@ -6,7 +6,8 @@ import System.IO
 main :: IO ()
 main = do
   -- hangman
-  nim
+  -- nim
+  life glider
 
 -- * hangman
 
@@ -124,3 +125,86 @@ playNim board player = do
 
 nim :: IO ()
 nim = playNim initial 1
+
+-- * life
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do
+  goto p
+  putStr xs
+
+goto :: Pos -> IO ()
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+width :: Int
+width = 10
+
+height :: Int
+height = 10
+
+type LifeBoard = [Pos]
+
+glider :: LifeBoard
+glider = [(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)]
+
+showcells :: LifeBoard -> IO ()
+showcells b = sequence_ [writeat p "O" | p <- b]
+
+isAlive :: LifeBoard -> Pos -> Bool
+isAlive b p = p `elem` b
+
+isEmpty :: LifeBoard -> Pos -> Bool
+isEmpty b p = not $ isAlive b p
+
+neighbs :: Pos -> [Pos]
+neighbs (x, y) =
+  map
+    wrap
+    [ (x - 1, y - 1),
+      (x, y - 1),
+      (x + 1, y - 1),
+      (x - 1, y),
+      (x + 1, y),
+      (x - 1, y + 1),
+      (x, y + 1),
+      (x + 1, y + 1)
+    ]
+
+wrap :: Pos -> Pos
+wrap (x, y) =
+  ( ((x - 1) `mod` width) + 1,
+    ((y - 1) `mod` height) + 1
+  )
+
+liveneighbs :: LifeBoard -> Pos -> Int
+liveneighbs b = length . filter (isAlive b) . neighbs
+
+survivors :: LifeBoard -> [Pos]
+survivors b = [p | p <- b, liveneighbs b p `elem` [2, 3]]
+
+births :: LifeBoard -> [Pos]
+births b =
+  [ p | p <- rmdups (concatMap neighbs b), isEmpty b p, liveneighbs b p == 3
+  ]
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x : xs) = x : rmdups (filter (/= x) xs)
+
+nextgen :: LifeBoard -> LifeBoard
+nextgen b = survivors b ++ births b
+
+life :: LifeBoard -> IO ()
+life b = do
+  cls
+  showcells b
+  wait 500000
+  life (nextgen b)
+
+wait :: Int -> IO ()
+wait n = sequence_ [return () | _ <- [1 .. n]]
