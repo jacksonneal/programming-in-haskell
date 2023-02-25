bbd5842d9def2e08e2bc169e4874a78fd655f4d7
jacksonneal <jacksontneal@gmail.com>
2023-02-24 19:36:38 -0500

    make it applicative

--- src/Ch13.hs
+++ src/Ch13.hs
@@ -24,3 +24,30 @@ item =
         [] -> []
         (x : xs) -> [(x, xs)]
     )
+
+instance Functor Parser where
+  -- fmap :: (a -> b) -> Parser a -> Parser b
+  fmap g p =
+    P
+      ( \inp ->
+          case parse p inp of
+            [] -> []
+            [(v, out)] -> [(g v, out)]
+      )
+
+instance Applicative Parser where
+  -- pure :: a -> Parser a
+  pure v = P (\inp -> [(v, inp)])
+
+  -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  pg <*> px =
+    P
+      ( \inp -> case parse pg inp of
+          [] -> []
+          [(g, out)] -> parse (fmap g px) out
+      )
+
+three :: Parser (Char, Char)
+three = g <$> item <*> item <*> item
+  where
+    g x y z = (x, z)
