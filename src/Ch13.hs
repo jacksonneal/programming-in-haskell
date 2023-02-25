8887e024f48dea892bd8423c111df642e7d10cb7
jacksonneal <jacksontneal@gmail.com>
2023-02-24 19:25:22 -0500

    item parser

--- src/Ch13.hs
+++ src/Ch13.hs
@@ -1,1 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Ch13 where
+
+{-
+A parser for things
+Is a function from string
+To lists of pairs
+Of things and strings
+-}
+
+import Control.Applicative
+import Data.Char
+
+newtype Parser a = P (String -> [(a, String)])
+
+parse :: Parser a -> String -> [(a, String)]
+parse (P p) = p
+
+item :: Parser Char
+item =
+  P
+    ( \case
+        [] -> []
+        (x : xs) -> [(x, xs)]
+    )
