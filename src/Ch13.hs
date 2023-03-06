9bbc60c6e40572e87c02aa5140d02d4b3782c82d
jacksonneal <jacksontneal@gmail.com>
2023-03-06 16:41:53 -0500

    ch13 exercises

--- src/Ch13.hs
+++ src/Ch13.hs
@@ -11,6 +11,7 @@ Of things and strings
 
 import Control.Applicative
 import Data.Char
+import System.IO
 
 newtype Parser a = P (String -> [(a, String)])
 
@@ -166,37 +167,166 @@ nats = do
   symbol "]"
   return (n : ns)
 
-expr :: Parser Int
+eol :: Char
+eol = '\n'
+
+comment :: Parser ()
+comment = do
+  symbol "--"
+  many (sat (/= eol))
+  return ()
+
+data Expr
+  = Val Int
+  | Add Expr Expr
+  | Sub Expr Expr
+  | Mult Expr Expr
+  | Div Expr Expr
+  | Expo Expr Expr
+  deriving (Show)
+
+expr :: Parser Expr
 expr =
   do
     t <- term
     do
       symbol "+"
-      e <- expr
-      return (t + e)
+      Add t <$> expr
+      <|> do
+        symbol "-"
+        Sub t <$> expr
       <|> return t
 
-term :: Parser Int
+term :: Parser Expr
 term =
   do
-    f <- factor
+    f <- exponential
     do
       symbol "*"
-      t <- term
-      return (f * t)
+      Mult f <$> term
+      <|> do
+        symbol "/"
+        Div f <$> term
       <|> return f
 
-factor :: Parser Int
+exponential :: Parser Expr
+exponential = do
+  b <- factor
+  do
+    symbol "^"
+    Expo b <$> exponential
+    <|> return b
+
+factor :: Parser Expr
 factor =
   do
     symbol "("
     e <- expr
     symbol ")"
     return e
-    <|> natural
+    <|> fmap Val integer
 
-eval :: String -> Int
-eval xs = case parse expr xs of
+evalA :: String -> Expr
+evalA xs = case parse expr xs of
   [(n, [])] -> n
   [(_, out)] -> error ("Unused input " ++ out)
   [] -> error "Invalid input"
+
+box :: [String]
+box =
+  [ "+---------------+",
+    "|               |",
+    "+---+---+---+---+",
+    "| q | c | d | = |",
+    "+---+---+---+---+",
+    "| 1 | 2 | 3 | + |",
+    "+---+---+---+---+",
+    "| 4 | 5 | 6 | - |",
+    "+---+---+---+---+",
+    "| 7 | 8 | 9 | * |",
+    "+---+---+---+---+",
+    "| 0 | ( | ) | / |",
+    "+---+---+---+---+"
+  ]
+
+buttons :: String
+buttons = standard ++ extra
+  where
+    standard = "qcd=123+456-789*0()/"
+    extra = "QCD \ESC\BS\DEL\n"
+
+type Pos = (Int, Int)
+
+goto :: Pos -> IO ()
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do
+  goto p
+  putStr xs
+
+showbox :: IO ()
+showbox = sequence_ [writeat (1, y) b | (y, b) <- zip [1 ..] box]
+
+display :: [Char] -> IO ()
+display xs = do
+  writeat (3, 2) (replicate 13 ' ')
+  writeat (3, 2) (reverse (take 13 (reverse xs)))
+
+getCh :: IO Char
+getCh = do
+  hSetEcho stdin False
+  x <- getChar
+  hSetEcho stdin True
+  return x
+
+calc :: String -> IO ()
+calc xs = do
+  display xs
+  c <- getCh
+  if c `elem` buttons
+    then process c xs
+    else do
+      beep
+      calc xs
+
+process :: Char -> String -> IO ()
+process c xs
+  | c `elem` "qQ\ESC" = quit
+  | c `elem` "dD\BS\DEL" = delete xs
+  | c `elem` "=\n" = eval xs
+  | c `elem` "cC" = clear
+  | otherwise = press c xs
+
+quit :: IO ()
+quit = goto (1, 14)
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+  [(n, [])] -> calc (show n)
+  _ ->
+    do
+      beep
+      calc xs
+
+beep :: IO ()
+beep = putStr "\BEL"
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+run :: IO ()
+run = do
+  cls
+  showbox
+  clear
